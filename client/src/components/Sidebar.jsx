e06c837d27fd41b9f4b7ed8339fff54c6faa7c06
akashkolakkal <kolakkalakash@gmail.com>
2023-03-29 16:54:11 +0530

    added side bar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,6 +4,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import { logo, sun } from '../assets';
 import { navlinks } from '../constants';
 
+import Model from './Canvas';
+
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
   <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
     {!isActive ? (
@@ -21,7 +23,10 @@ const Sidebar = () => {
   return (
     <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
       <Link to="/">
-        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
+        {/* <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} /> */}
+        <div className='w-[62px] h-[62px] bg-[#2c2f32] rounded-[10px]'>
+          <Model/>
+        </div>
       </Link>
 
       <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
